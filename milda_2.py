acb98093781fa61f9eca0d2d3ad897e8db49379e
dhp-byte <djaoyang.habekreo@gmail.com>
2026-02-17 10:58:27 +0100

    Update milda_2.py

--- milda_2.py
+++ milda_2.py
@@ -7,6 +7,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib
+import scipy
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
