5948f123032ce46c64a8a7cf4458ff652852869b
dhp-byte <djaoyang.habekreo@gmail.com>
2026-02-20 12:56:06 +0100

    Update milda_2.py

--- milda_2.py
+++ milda_2.py
@@ -2200,7 +2200,7 @@ def process_raw_kobo_data(df):
     # GÉNÉRATION DES COLONNES MANQUANTES (Cause de la KeyError)
     df['indic_servi'] = (df['menage_servi'] == 'Oui').astype(int)
     df['indic_correct'] = (df.get('norme') == 'Oui').astype(int)
-    df['indic_marque'] = ((df.get('menage_marque') == 'Oui').astype(int)
+    df['indic_marque'] = (df.get('menage_marque') == 'Oui').astype(int)
     df['indic_info'] = (df.get('information') == 'Oui').astype(int)
     
     # Calcul des écarts pour la page analyse
